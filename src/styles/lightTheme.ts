e745fbacc57684b103e5b9375b98928d1eab84e4
unknown <caiohbe@gmail.com>
2025-08-20 16:01:55 -0300

    feat(theme): add lightTheme using designTokens

---
+++ src/styles/lightTheme.ts
@@ -0,0 +1,20 @@
+import type { DefaultTheme } from "styled-components"
+import { designTokens } from "./designTokens"
+
+export const lightTheme: DefaultTheme = {
+  ...designTokens,
+  colors: {
+    primary: "oklch(0.7 0.2 250)",
+    secondary: "oklch(0.7 0.2 300)",
+    background: {
+      dark: "oklch(0.9 0 0)",
+      default: "oklch(0.95 0 0)",
+      light: "oklch(1 0 0)",
+    },
+    text: {
+      primary: "oklch(0.05 0 0)",
+      secondary: "oklch(0.3 0 0)",
+      inverse: "oklch(0.95 0 0)",
+    },
+  },
+}
