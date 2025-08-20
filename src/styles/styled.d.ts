b75f95fa96c6644fa5d8f38dcf2383199d06e703
unknown <caiohbe@gmail.com>
2025-08-20 14:29:05 -0300

    chore(types): declaration file 'styled.d.ts' for DefaultTheme from styled-components

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,32 @@
+import { DefaultTheme } from "styled-components"
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: string
+      secondary: string
+      background: string
+      surface: string
+      text: {
+        primary: string
+        secondary: string
+        inverse: string
+      }
+    }
+    typography: {
+      fontFamily: string
+      sizes: {
+        small: string
+        base: string
+        large: string
+        xLarge: string
+      }
+      weight: {
+        regular: number
+        medium: number
+        bold: number
+      }
+    }
+    spacing: (factor: number) => string
+  }
+}
