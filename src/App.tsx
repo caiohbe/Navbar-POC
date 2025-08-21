23ed828c290b1504331eebf23967a4fd629c6e9d
unknown <caiohbe@gmail.com>
2025-08-21 17:16:29 -0300

    feature(routing): added react router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import { NavBar } from "./NavBar"
 import { ResetStyle } from "./styles/ResetStyle"
+import { Home } from "./pages/Home"
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <ResetStyle />
-      APP
-    </>
+      <NavBar /> {/*FIXME*/}
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
